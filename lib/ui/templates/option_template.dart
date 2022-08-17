96493b0706b78f35cd327631a9c67eae1bf02f0f
Sportkanone123 <kim-mueller@gmx.com>
2022-08-17 17:54:40 +0200

    Added Option Template and Page Entry Template

---
+++ lib/ui/templates/option_template.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class OptionTemplate extends StatelessWidget {
+  final IconData icon;
+  final Color iconColor;
+  final String title;
+  final String description;
+  final Widget routeTo;
+
+  const OptionTemplate({Key? key, required this.icon, required this.iconColor, required this.title, required this.description, required this.routeTo})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: (){
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => routeTo),
+        );
+      },
+      child: Row(
+        children: [
+          Container(
+            height: 36,
+            width: 36,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: iconColor,
+            ),
+            child: Icon(icon, size: 24, color: Colors.white,),
+          ),
+          const SizedBox(width: 10,),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title, style: const TextStyle(fontSize: 16, color: Colors.black),),
+                const SizedBox(height: 2,),
+                Text(description, style: const TextStyle(fontSize: 12, color: Color(0xFF84848A),),),
+              ],
+            ),
+          ),
+          const Text("▶", style: TextStyle(fontSize: 20, color: Color(0xFF84848A), fontWeight: FontWeight.bold),),
+        ],
+      )
+    );
+  }
+}
---
+++ lib/ui/templates/page_entry_template.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:smart_finance/ui/constants.dart';
+
+class PageEntry extends StatelessWidget {
+  final String title;
+  final Widget child;
+
+  const PageEntry({Key? key, required this.title, required this.child})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(kDefaultPadding, kDefaultPadding * 1.5, kDefaultPadding, kDefaultPadding / 2),
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(bottom: kDefaultPadding / 2),
+            alignment: Alignment.centerLeft,
+            child: Text(title, style: const TextStyle(fontSize: 20, color: Colors.black),),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: kDefaultPadding / 2),
+            padding: const EdgeInsets.all(10),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+              color: Colors.white,
+            ),
+            child: child,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PageEntryBoldTitle extends StatelessWidget {
+  final String title;
+  final Widget child;
+
+  const PageEntryBoldTitle({Key? key, required this.title, required this.child})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(kDefaultPadding, kDefaultPadding * 1.5, kDefaultPadding, kDefaultPadding / 2),
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(bottom: kDefaultPadding / 2),
+              alignment: Alignment.centerLeft,
+              child: Text(title, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: kDefaultPadding / 2),
+              padding: const EdgeInsets.all(10),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+                color: Colors.white,
+              ),
+              child: child,
+            ),
+          ],
+        ),
+    );
+  }
+}
+
+class PageEntryExtendOption extends StatelessWidget {
+  final String title;
+  final Widget child;
+  final Widget extendWidget;
+
+  const PageEntryExtendOption({Key? key, required this.title, required this.child, required this.extendWidget})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(kDefaultPadding, kDefaultPadding * 1.5, kDefaultPadding, kDefaultPadding / 2),
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(bottom: kDefaultPadding / 2),
+            alignment: Alignment.centerLeft,
+            child: Row(
+              children: [
+                Expanded(child: Text(title, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),),),
+                InkWell(
+                  onTap: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => extendWidget),
+                    );
+                  },
+                  child: const Text("See all ▶", style: TextStyle(fontSize: 16, color: Colors.lightBlue)),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: kDefaultPadding / 2),
+            padding: const EdgeInsets.all(10),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+              color: Colors.white,
+            ),
+            child: child,
+          ),
+        ],
+      ),
+    );
+  }
+}
