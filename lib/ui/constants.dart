25e8ee410391615eb066b8ee84ac3318fe9fad9f
Sportkanone123 <kim-mueller@gmx.com>
2022-08-17 17:53:42 +0200

    Added page router without animation.

--- lib/ui/constants.dart
+++ lib/ui/constants.dart
@@ -5,4 +5,4 @@ const kBackgroundColor = Color(0xFFF2F2F6);
 const kTextColorPrimary = Color(0xFF2f3783);
 const kTextColorBackground = Color(0xFFCCCCCC);
 
-const double kDefaultPadding = 25.0;
+const double kDefaultPadding = 12.0;
